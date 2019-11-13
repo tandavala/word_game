52b97c619530c09dd9297c88c8131898f48bbbd1
tandavala <jose.tandavala@gmail.com>
2019-11-13 08:59:32 +0100

    add a stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:random_words/random_words.dart';
 
@@ -7,15 +9,32 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final noun = new WordNoun.random();
+    final adjective = WordAdjective.random();
+
     return MaterialApp(
       title: 'Word Game',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Word Game"),
-        ),
-        body: Center(
-          child: Text('Noun: ${noun.asCapitalized}'),
-        ),
+      home: RandomSentences(),
+    );
+  }
+}
+
+class RandomSentences extends StatefulWidget {
+  @override
+  createState() => _RandomSentecesState();
+}
+
+class _RandomSentecesState extends State<RandomSentences> {
+  @override
+  final adjective = WordAdjective.random();
+  final noun = WordNoun.random();
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Word Game"),
+      ),
+      body: Center(
+        child: Text("Ola ${noun.asCapitalized}"),
       ),
     );
   }
