af2ef43b75a6de79f28b0d2da98671128e2eeea4
tandavala <jose.tandavala@gmail.com>
2019-11-13 08:46:11 +0100

    add random_words package

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:random_words/random_words.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final noun = new WordNoun.random();
     return MaterialApp(
       title: 'Word Game',
-      home: Text("ola"),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Word Game"),
+        ),
+        body: Center(
+          child: Text('Noun: ${noun.asCapitalized}'),
+        ),
+      ),
     );
   }
 }
